922f7b86b4705e5ad6ca14b35caf471f2cba4a11
Anders Ramsay <andersr@gmail.com>
2016-08-12 15:23:44 -0400

    cleanup

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 var path = require('path');
 var express = require('express');
 var app = express();
@@ -9,15 +8,12 @@ if(env == 'staging'){
   app.use(basicAuth(process.env.NPM_CONFIG_BASIC_AUTH_USER, process.env.NPM_CONFIG_BASIC_AUTH_PWD));
 }
 
-// process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 3000;
 
-// set the view engine to ejs
 app.set('views', path.join(__dirname, '/dist/views'));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/dist/public'));
 
-// set the home page
 app.get('/', function(req, res) {
 	res.render('index');
 });
