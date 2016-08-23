a43ac5342731bc6fc8cb84b552213a30709a5f79
Anders Ramsay <andersr@gmail.com>
2016-08-23 10:44:26 -0400

    rm semi from linting

--- src/scripts/dev/obfuscate_email.js
+++ src/scripts/dev/obfuscate_email.js
@@ -1,48 +1,42 @@
 /*! Email obfuscator script 2.1 by Tim Williams, Andrew Moulden, converted to jquery, ttp://www.jottings.com/obfuscator/ */
 
-$(function() {
-
-	'use strict';
+$(function () {
+  'use strict'
 
   const config = {
-  	id: '#andersco-mailto',
-  	coded: 'ca@XrLnqI.pN',
-  	key: 'wmS20BDlarV8QdAMbC1jXqUgHLzhfvRts7KG3ZWcx5EoIOkeP4Tp6FuNYJin9y'
-  };
+    id: '#andersco-mailto',
+    coded: 'ca@XrLnqI.pN',
+    key: 'wmS20BDlarV8QdAMbC1jXqUgHLzhfvRts7KG3ZWcx5EoIOkeP4Tp6FuNYJin9y'
+  }
 
-  function getEmailAddress(options){
+  function getEmailAddress (options) {
     let
-     shift=options.coded.length,
-     address='',
-     ltr
-   ;
+      shift = options.coded.length,
+      address = '',
+      ltr
 
-   for (var i=0; i<options.coded.length; i++) {
-      if (options.key.indexOf(options.coded.charAt(i))==-1) {
-        ltr = options.coded.charAt(i);
-        address += (ltr);
-      }
-      else {
-        ltr = (options.key.indexOf(options.coded.charAt(i))-shift+options.key.length) % options.key.length;
-        address += (options.key.charAt(ltr));
+    for (let i = 0; i < options.coded.length; i++) {
+      if (options.key.indexOf(options.coded.charAt(i)) === -1) {
+        ltr = options.coded.charAt(i)
+        address += (ltr)
+      } else {
+        ltr = (options.key.indexOf(options.coded.charAt(i)) - shift + options.key.length) % options.key.length
+        address += (options.key.charAt(ltr))
       }
     }
-    return address;
+    return address
   }
 
+  function createEmailLink (options) {
+    let
+      address = getEmailAddress(options),
+      linkText = options.linkText || address,
+      $link = $(options.id),
+      mailto = 'mailto:' + address
 
-  function createEmailLink(options){
-
-    var
-      address    = getEmailAddress(options),
-      linkText   = options.linkText || address,
-      $link      = $(options.id),
-      mailto     = 'mailto:'+address
-    ;
-
-    $link.attr('href',mailto).text(linkText);
+    $link.attr('href', mailto).text(linkText)
   }
 
-  createEmailLink(config);
+  createEmailLink(config)
 
-});
+})
