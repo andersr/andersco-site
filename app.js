d7a2622f3aefd668a211664c920d24fca160c1e0
Anders Ramsay <andersr@gmail.com>
2016-10-14 11:57:20 -0400

    centered landing page info

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-var path = require('path');
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
-var env = process.env.NODE_ENV;
+var path = require('path')
+var express = require('express')
+var app = express()
+var port = process.env.PORT || 3000
+var env = process.env.NODE_ENV
 
-if(env == 'staging'){
-  var basicAuth = require('basic-auth-connect');
-  app.use(basicAuth(process.env.NPM_CONFIG_BASIC_AUTH_USER, process.env.NPM_CONFIG_BASIC_AUTH_PWD));
+if (env === 'staging') {
+  var basicAuth = require('basic-auth-connect')
+  app.use(basicAuth(process.env.NPM_CONFIG_BASIC_AUTH_USER, process.env.NPM_CONFIG_BASIC_AUTH_PWD))
 }
 
-app.set('views', path.join(__dirname, '/dist/views'));
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/dist/public'));
+app.set('views', path.join(__dirname, '/dist/views'))
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/dist/public'))
 
-app.get('/', function(req, res) {
-	res.render('index');
-});
+app.get('/', function (req, res) {
+  res.render('index')
+})
 
-app.listen(port, function() {
-	console.log('App running at http://localhost:' + port);
-});
+app.listen(port, function () {
+  console.log('App running at http://localhost:' + port)
+})
