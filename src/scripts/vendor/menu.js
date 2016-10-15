5703295c57f6047304df1bccec1521607f853193
Anders Ramsay <andersr@gmail.com>
2016-10-15 09:47:46 -0400

    go to section on click

--- src/scripts/vendor/menu.js
+++ src/scripts/vendor/menu.js
@@ -55,7 +55,7 @@
     this.wrapper = document.querySelector(this.options.wrapper);
     this.mask = document.querySelector(this.options.maskClass);
     this.menu = document.querySelector('#c-menu--' + this.options.type);
-    this.closeBtn = this.menu.querySelector('.c-menu__close');
+    this.closeMenu = this.menu.querySelectorAll('.c-menu__close');
     this.menuOpeners = document.querySelectorAll(this.options.menuOpenerClass);
     this._initEvents();
   };
@@ -65,10 +65,13 @@
    */
   Menu.prototype._initEvents = function() {
     // Event for clicks on the close button inside the menu.
-    this.closeBtn.addEventListener('click', function(e) {
-      e.preventDefault();
-      this.close();
-    }.bind(this));
+    for (var i = 0; i < this.closeMenu.length; i++) {
+      this.closeMenu[i].addEventListener('click', function(e) {
+        e.preventDefault();
+        this.close();
+      }.bind(this));
+   }
+
 
     // Event for clicks on the mask.
     this.mask.addEventListener('click', function(e) {
