8bc0214a7cd41a32521e5795dbdeabbc085751f5
Anders Ramsay <andersr@gmail.com>
2016-10-18 11:49:30 -0400

    sending message on submit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
+require('dotenv').config()
 var path = require('path')
 var express = require('express')
-var nodemailer = require('nodemailer')
 var app = express()
 var port = process.env.PORT || 3000
 var env = process.env.NODE_ENV
@@ -10,9 +10,49 @@ if (env === 'staging') {
   app.use(basicAuth(process.env.NPM_CONFIG_BASIC_AUTH_USER, process.env.NPM_CONFIG_BASIC_AUTH_PWD))
 }
 
+
+// FORMS
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true
+}))
+
+
+// MAIL
+var nodemailer = require('nodemailer')
+var mailgun = require('nodemailer-mailgun-transport')
+var auth = {
+  auth: {
+    api_key: process.env.MAILGUN_API,
+    domain: process.env.MAILGUN_DOMAIN
+  }
+}
+var nodemailerMailgun = nodemailer.createTransport(mailgun(auth))
+
+
 app.set('views', path.join(__dirname, '/dist/views'))
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/dist/public'))
+
+app.post('/mail', function(req, res) {
+  res.setHeader('Content-Type', 'application/json')
+  console.log('body: ', req.body)
+  nodemailerMailgun.sendMail({
+    from: req.body.email,
+    to: process.env.MAILGUN_SEND_TO,
+    subject: 'Message from ' + req.body.name ,
+    text: req.body.message
+  }, function (err, info) {
+    if (err) {
+      console.log('Error: ' + err);
+    }
+    else {
+      console.log('Response: ' + info);
+    }
+  })
+  res.end()
+})
+
 
 app.get('/', function (req, res) {
   res.render('index')
@@ -21,3 +61,33 @@ app.get('/', function (req, res) {
 app.listen(port, function () {
   console.log('App running at http://localhost:' + port)
 })
+
+    // nodemailerMailgun.sendMail({
+    //   from: 'myemail@example.com',
+    //   to: 'anders@anders.co', // An array if you have multiple recipients.
+    //   subject: 'AndersCo Test Two',
+    //   html: '<b>HTML Test</b>',
+    //   //You can use "text:" to send plain-text content. It's oldschool!
+    //   text: 'Mailgun test'
+    // }, function (err, info) {
+    //   if (err) {
+    //     console.log('Error: ' + err);
+    //   }
+    //   else {
+    //     console.log('Response: ' + info);
+    //   }
+    // });
+
+    // res.send(req.body);
+      // res.setHeader('Content-Type', 'application/json');
+      // get form values
+      // validate form values
+      // if valid, send, else return error
+
+        // var name = req.body.name;
+        // res.send(JSON.stringify({
+        //   name: req.body.name || null,
+        //   email: req.body.email || null
+        // }));
+
+        	// console.log('from express: ' + req.body.name + ', Email: ' + req.body.email);
---
+++ src/scripts/dev/form-handler.js
@@ -0,0 +1,59 @@
+// src: https://github.com/kevinchisholm/handling-POST-requests-with-express-and-node.js
+
+(function () {
+
+  function init(){
+    $('#submitButton').click(submitButtonHandler)
+  }
+
+  function submitButtonHandler (evt) {
+      evt.preventDefault()
+      var data = {}
+					data.name = $('#name').val()
+					data.email =  $('#email').val()
+          data.message =  $('#message').val()
+
+      //make the AJAX call
+      $.post({
+        url: '/mail',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        success: function () {
+          console.log('success')
+          // console.log(JSON.stringify(data))
+        }
+      })
+  }
+
+  // function postSuccessHandler (jsonData) {
+  //   console.log('success: ', jsonData)
+  // }
+
+//init on document ready
+$(document).ready(init)
+})()
+
+// evt.stopPropagation()
+
+// $('#post-results-container').fadeOut()
+// $('.ajaxLoader').css('display', 'inline-block')
+// var $name = $('#name').val()
+// var $email = $('#email').val()
+// var $email = $('#email').val()
+//
+// console.log('name: ', $name )
+
+// var $data = $('#post-results-container .data')
+//
+// //reset the UI
+// $data.html('')
+// $('.ajaxLoader').hide()
+
+//update the UI with the data returned from the AJAX call
+// $.each(jsonData, function (key, val) {
+//   console.log(key, ':', val)
+//   // $data.append('<li><b>' +  key + '</b>'   + val + '</li>')
+// })
+
+// $('#post-results-container').fadeIn()
