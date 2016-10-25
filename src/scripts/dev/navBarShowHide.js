506ba289904c34ec87aa188c5fe12a547a3eb892
Anders Ramsay <andersr@gmail.com>
2016-10-25 13:36:00 -0400

    show/hide header nav on resize

--- src/scripts/dev/navBarShowHide.js
+++ src/scripts/dev/navBarShowHide.js
@@ -2,24 +2,37 @@
   $(function () {
     'use strict'
 
-  var $navBar = $('.navbar')
-  var $headerNav = $('.header-nav')
-  var $welcomeMsg = $('#welcome-msg')
-  var $welcomeNav = $('#welcome-nav')
-  var navbarHeight = $navBar.outerHeight()
 
-  $(window).scroll(function() {
-    var scrollPosition = $(window).scrollTop()
-    var welcomeMsgLocation = ($welcomeMsg.offset().top - scrollPosition)
-    var welcomeNavLocation = ($welcomeNav.offset().top - scrollPosition) + 25
+    var $navBar = $('.navbar')
+    var $headerNav = $('.header-nav')
+    var $topContent = $('.top-content')
+    var $welcomeNav = $('#welcome-nav')
+    var navbarHeight = $navBar.outerHeight()
+    var windowWidth = $( window ).width();
+    var tabletBreakpoint = 640 // matches $breakpoint-tablet in _variables.scss
+    $(window).resize(function() {
+      windowWidth = $( window ).width()
+      if (windowWidth > tabletBreakpoint) {
+       $headerNav.show()
+      }
+      else {
+        $headerNav.hide()
+      }
+    })
 
-    if (welcomeMsgLocation < navbarHeight) {
-     $navBar.addClass('drop-shadow')
-    }
-    else {
-      $navBar.removeClass('drop-shadow')
-    }
-      if (welcomeNavLocation < navbarHeight) {
+    $(window).scroll(function() {
+      var scrollPosition = $(window).scrollTop()
+      var welcomeMsgLocation = ($topContent.offset().top - scrollPosition)
+      var welcomeNavLocation = ($welcomeNav.offset().top - scrollPosition) + 25 // Why is the additional 25 needed?
+
+      if (welcomeMsgLocation < navbarHeight) {
+       $navBar.addClass('drop-shadow')
+      }
+      else {
+        $navBar.removeClass('drop-shadow')
+      }
+
+      if (welcomeNavLocation < navbarHeight && windowWidth > tabletBreakpoint) {
        $headerNav.show()
       }
       else {
