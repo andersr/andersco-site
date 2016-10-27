1af89d4fcc2f030af90debf11aa6642850b4d5e4
Anders Ramsay <andersr@gmail.com>
2016-10-27 11:32:22 -0400

    basic about section styling

---
+++ src/scripts/dev/dropshadowOnScroll.js
@@ -0,0 +1,14 @@
+// $(function() {
+//    var $el = $('.drop-shadow-on-scroll')
+//    console.log('top: ', $(document).scrollTop())
+//    console.log('html: ', $('html').offset().top)
+// 
+//    $(window).scroll(function() {
+//       console.log('top: ', $(document).scrollTop())
+//       console.log('html: ', $('html').offset().top)
+//      var scrollPosition = $(window).scrollTop()
+//      $el.toggleClass(function(){
+//         return scrollPosition > 0 ? 'drop-shadow' : null
+//      })
+//    })
+// })
